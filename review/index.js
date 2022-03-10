d20b78f7f54e794326396f8f51097d0565f2b92f
varshasuba <101315409+varshasuba@users.noreply.github.com>
2022-03-10 15:53:27 +0530

    Add files via upload

---
+++ review/index.js
@@ -0,0 +1,29 @@
+const sequelize = require("./utils/database");
+const Customer = require("./models/customer");
+const Order = require("./models/order");
+
+Customer.hasMany(Order);
+
+let customerId = null;
+sequelize
+  .sync({force: true})
+  // .sync()
+  .then((result) => {
+    return Customer.create({name: "varsha", email: "vp@gmail.com"})
+    console.log(result);
+  })
+  .then(customer => {
+    customerId = customer.id;
+    console.log("First Customer Created: ",customer);
+    return customer.createOrder({total: 45});
+  })
+  .then(order => {
+    console.log("Order is : ",order);
+    return Order.findAll({ where: customerId});
+  })
+  .then(orders => {
+    console.log("All the Orders are : ",orders);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
